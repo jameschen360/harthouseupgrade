8945b0d64d807e852b8d255082e7057e4d85eadf
James Chen <jxchen@live.ca>
2017-11-18 21:30:25 -0700

    font changes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FrontFetchService } from '../server/front-fetch.service';
+
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  postData = {
+    'user_id': 'public'
+  };
+  public responseData;
+  public parrallaxUrl;
+  public aboutUsContent;
+  constructor(public getData: FrontFetchService) {
+    this.getAboutUsContent();
+  }
 
   ngOnInit() {
   }
 
+  getAboutUsContent() {
+    this.getData.postData(this.postData, 'aboutUsPage').then((result) => {
+      this.responseData = result;
+      this.parrallaxUrl = this.responseData.imagePath;
+      this.aboutUsContent = this.responseData.content;
+    }, (err) => {
+    });
+  }
 }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FrontFetchService } from '../server/front-fetch.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  postData = {
+    'user_id': 'public'
+  };
+  public responseData;
+  public menus;
+  public parrallaxUrl;
+  constructor(public getData: FrontFetchService, private sanitizer: DomSanitizer) {
+    this.getBannerImages();
+  }
 
   ngOnInit() {
+  }
+
+  getBannerImages() {
+    this.getData.postData(this.postData, 'menuPage').then((result) => {
+      this.responseData = result;
+      this.menus = this.responseData.menus;
+      this.parrallaxUrl = this.responseData.imagePath;
+
+
+    }, (err) => {
+    });
   }
 
 }
