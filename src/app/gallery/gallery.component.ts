a1f45d3f2a48bc2f81c0a0382ebc7a21cc2438e6
David Yang <zjyang@ualberta.ca>
2017-11-21 21:52:41 -0700

    just added in the new gallery component

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -26,7 +26,9 @@ export class GalleryComponent implements OnInit {
   getGalleryImages() {
     this.getData.postData(this.postData, 'galleryImages').then((result) => {
       this.responseData = result;
-      this.galleryImages = this.responseData.galleryImages;
+      this.galleryImages[0].small = this.responseData.galleryImages;
+      // this.galleryImages[0].medium = this.responseData.galleryImages;
+      // this.galleryImages[0].big = this.responseData.galleryImages;
       console.log(this.responseData.galleryImages);
       console.log('hello');
       
