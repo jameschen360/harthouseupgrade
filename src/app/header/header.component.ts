c264c3e1989fb8eaab0e21ef75b323b8b5ec8299
James Chen <jxchen@live.ca>
2017-11-26 21:34:10 -0700

    final

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+
+declare var $;
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
 
-  constructor() { }
+export class HeaderComponent implements OnInit {
+  // @ViewChild('isOpen') isOpen: ElementRef;
+
+  constructor() {
+    $(function() {
+      $(document).click(function (event) {
+        $('.navbar-collapse').collapse('hide');
+      });
+    });
+  }
 
   ngOnInit() {
   }
+
+
 }
