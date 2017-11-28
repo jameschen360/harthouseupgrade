aa5c873d0618ec13696f4493aa7e43457bda1137
James Chen <jxchen@live.ca>
2017-11-27 23:03:20 -0700

    hambergur

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,18 +12,33 @@ declare var $;
 export class HeaderComponent implements OnInit {
 
   constructor(private router: Router) {
-    $(function () {
-      $(document).click(function (event) {
-        $('.navbar-collapse').collapse('hide');
-      });
-    });
-
-
   }
 
   ngOnInit() {
     this.router.events.subscribe((val) => {
       window.scrollTo(0, 0);
+    });
+
+    // $(document).ready(function () {
+    //   $('#nav-icon').click(function () {
+    //     $('#nav-icon').toggleClass('open');
+    //   });
+
+    // });
+    $(function () {
+      $(document).click(function (event) {
+        if ($('#nav-icon').data('clicked') && !$('#ham').hasClass('collapsed')) {
+          $('#nav-icon').toggleClass('open');
+        } else if ( event.target.class === 'container' && $('#ham').hasClass('collapsed')) {
+
+
+        } else if (event.target.class !== 'container' && $('#ham').hasClass('collapsed')) {
+
+        } else {
+
+        }
+        $('.navbar-collapse').collapse('hide');
+      });
     });
   }
 
